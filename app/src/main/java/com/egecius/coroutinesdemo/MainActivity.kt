7bb002b857d307ff9ed57d28cf4fa73a75698c4f
Egidijus Gecius <e.gecius@gmail.com>
2020-03-20 19:10:31 +0000

    add another coroutine that's called on the main thread

--- app/src/main/java/com/egecius/coroutinesdemo/MainActivity.kt
+++ app/src/main/java/com/egecius/coroutinesdemo/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,16 +21,23 @@ class MainActivity : AppCompatActivity() {
     private fun setClickListener() {
         button.setOnClickListener {
             CoroutineScope(IO).launch {
-                fakeApiRequest()
+                fetchFakeApiRequestAndUpdateUi()
             }
         }
     }
 
-    private suspend fun fakeApiRequest() {
-        val result1 = getResult1FromApi()
-        println("debug: $result1")
+    private suspend fun setTextOnMainThread(input: String) {
+        withContext(Main) {
+            val newText = text_view.text.toString() + "\n" + input
+            text_view.text = newText
+        }
     }
 
+    private suspend fun fetchFakeApiRequestAndUpdateUi() {
+        val result1 = getResult1FromApi()
+        println("debug: $result1")
+        setTextOnMainThread(result1)
+    }
 
     private suspend fun getResult1FromApi(): String {
         logThread("getResult1FromApi")
