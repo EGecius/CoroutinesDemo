ba0d1b4ddbc1c372e47a6ddb415972f0fbd9582c
Egidijus Gecius <e.gecius@gmail.com>
2020-03-20 20:43:38 +0000

    add JobActivity.kt

---
+++ app/src/main/java/com/egecius/coroutinesdemo/JobActivity.kt
@@ -0,0 +1,66 @@
+package com.egecius.coroutinesdemo
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_job.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class JobActivity : AppCompatActivity() {
+
+    lateinit var job : Job
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job)
+        initProgressBar()
+        setupListener()
+        initJob()
+    }
+
+    private fun initProgressBar() {
+        progressBar.progress = START
+        progressBar.max = MAX
+    }
+
+    private fun setupListener() {
+        button.setOnClickListener {
+        	startProgress()
+        }
+    }
+
+    private fun startProgress() {
+        CoroutineScope(IO + job).launch {
+            for (i in START..MAX) {
+                progressBar.progress = i
+            }
+        }
+    }
+
+    private fun initJob() {
+        if (!::job.isInitialized) {
+            job = Job()
+            job.invokeOnCompletion {
+                showToast("complete")
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        GlobalScope.launch(Main) {
+            Toast.makeText(this@JobActivity, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        const val START = 0
+        const val MAX = 100
+    	const val TIME_IN_MS = 4000
+
+    }
+}
