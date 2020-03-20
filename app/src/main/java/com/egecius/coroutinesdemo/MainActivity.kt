1635b1249b20787f10698f8a473ce6a70f43a6ba
Egidijus Gecius <e.gecius@gmail.com>
2020-03-20 18:53:01 +0000

    add 'suspend method'

--- app/src/main/java/com/egecius/coroutinesdemo/MainActivity.kt
+++ app/src/main/java/com/egecius/coroutinesdemo/MainActivity.kt
@@ -2,11 +2,31 @@ package com.egecius.coroutinesdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+
+        }
+    }
+
+
+    private suspend fun getResult1FromApi(): String {
+        logThread("getResult1FromApi")
+        delay(1000)
+        return RESULT_1
+    }
+
+    private fun logThread(methodName: String) {
+        println("debug: ${methodName}: ${Thread.currentThread().name}")
+    }
+
+    companion object {
+        private const val RESULT_1 = "Result 1"
     }
 }
