d35f109c1699a9ad3a7c700225625dfe3a213362
Egidijus Gecius <e.gecius@gmail.com>
2020-03-26 14:55:58 +0000

    shows how to use scope builder

--- app/src/test/java/com/egecius/coroutinesdemo/BasicsTest.kt
+++ app/src/test/java/com/egecius/coroutinesdemo/BasicsTest.kt
@@ -43,4 +43,24 @@ class BasicsTest {
         }
         println("yes, they run in parallel")
     }
+
+    @Test
+    fun `shows how to use scope builder`() = runBlocking { // this: CoroutineScope
+        launch {
+            delay(200L)
+            println("Task from runBlocking")
+        }
+
+        coroutineScope { // Creates a coroutine scope
+            launch {
+                delay(500L)
+                println("Task from nested launch")
+            }
+
+            delay(100L)
+            println("Task from coroutine scope") // This line will be printed before the nested launch
+        }
+
+        println("Coroutine scope is over") // This line is not printed until the nested launch completes
+    }
 }
