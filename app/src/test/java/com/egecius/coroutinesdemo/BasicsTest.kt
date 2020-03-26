820f2c56fc864ebd3d18aab8865b73ba0c923200
Egidijus Gecius <e.gecius@gmail.com>
2020-03-26 14:15:11 +0000

    having 100k coroutines complete takes less than 1s

--- app/src/test/java/com/egecius/coroutinesdemo/BasicsTest.kt
+++ app/src/test/java/com/egecius/coroutinesdemo/BasicsTest.kt
@@ -1,9 +1,10 @@
 package com.egecius.coroutinesdemo
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class BasicsTest {
 
     @Test
@@ -16,5 +17,18 @@ class BasicsTest {
         }
 
         println("end of test")
+    }
+
+    @Test
+    fun `having 100k coroutines complete takes less than 1s`() = runBlockingTest {
+        val deferredList: List<Deferred<Int>> = (1..100_000).map { n ->
+            GlobalScope.async {
+                n
+            }
+        }
+
+        val sum = deferredList.map { it.await().toLong() }.sum()
+
+        println(sum)
     }
 }
