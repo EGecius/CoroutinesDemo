cc69fd1430b16aabe893e4a0c4d7e87f58ca4e49
Egidijus Gecius <e.gecius@gmail.com>
2020-03-26 09:59:33 +0000

    with supervisor job when a child coroutine fails, siblings don't get cancelled

--- app/src/test/java/com/egecius/coroutinesdemo/SupervisorScopeDemoTest.kt
+++ app/src/test/java/com/egecius/coroutinesdemo/SupervisorScopeDemoTest.kt
@@ -1,23 +1,39 @@
 package com.egecius.coroutinesdemo
 
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.supervisorScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Ignore
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class SupervisorScopeDemoTest {
+
+    @Test
+    @Ignore // for some reason it does not work as it's supposed to. second coroutine does not finish after delay
+    fun `with supervisor job when a child coroutine fails, siblings don't get cancelled`() = runBlockingTest {
+
+        val coroutineScope = CoroutineScope(SupervisorJob())
+        coroutineScope.launch {
+            println("starting child1")
+            throw Exception("Egis")
+        }
+
+        coroutineScope.launch {
+            println("starting child2")
+            delay(100)
+            println("completed child2")
+        }
+    }
+
 
     @Test(expected = Exception::class)
     fun `when a child coroutine in supervisor scope fails, siblings don't get cancelled`() = runBlockingTest {
 
         launch {
             supervisorScope {
-                println("starting child1")
 
                 val child1 = launch {
+                    println("starting child1")
                     delay(100)
                     throw Exception("Egis")
                 }
