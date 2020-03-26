2fc20369a49f3d195011014f3579e2a988e7ad48
Egidijus Gecius <e.gecius@gmail.com>
2020-03-26 16:10:00 +0000

    coroutines execute sequentially by default

---
+++ app/src/test/java/com/egecius/coroutinesdemo/ComposingSuspendingFunctionsTest.kt
@@ -0,0 +1,35 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
+package com.egecius.coroutinesdemo
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import kotlin.system.measureTimeMillis
+
+@ExperimentalCoroutinesApi
+class ComposingSuspendingFunctionsTest {
+
+    @Test
+    fun `coroutines execute sequentially by default`() = runBlocking {
+
+        val time = measureTimeMillis {
+            val one = doSomethingUsefulOne()
+            val two = doSomethingUsefulTwo()
+            println("The answer is ${one + two}")
+        }
+        println("Completed in $time ms")
+
+    }
+
+    suspend fun doSomethingUsefulOne(): Int {
+        delay(100) // pretend we are doing something useful here
+        return 13
+    }
+
+    suspend fun doSomethingUsefulTwo(): Int {
+        delay(200) // pretend we are doing something useful here, too
+        return 29
+    }
+}
