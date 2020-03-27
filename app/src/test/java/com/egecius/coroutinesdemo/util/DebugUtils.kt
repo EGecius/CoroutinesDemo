9297a90887c725614e76263afaea8f5ff35eaa0e
Egidijus Gecius <e.gecius@gmail.com>
2020-03-27 10:02:06 +0000

    print only job instead of the whole coroutine context with duplicate and redundant info

--- app/src/test/java/com/egecius/coroutinesdemo/util/DebugUtils.kt
+++ app/src/test/java/com/egecius/coroutinesdemo/util/DebugUtils.kt
@@ -1,9 +1,11 @@
 package com.egecius.coroutinesdemo.util
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 
 fun CoroutineScope.log(msg: String) {
     val threadName = Thread.currentThread().name
-    val fullMsg = "$threadName, $coroutineContext: + $msg"
+    val job = println(coroutineContext[Job])
+    val fullMsg = "$threadName, $job: + $msg"
     println(fullMsg)
 }
