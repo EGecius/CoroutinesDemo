51ece95edb715ecff582af87c86f0b41dc3a54be
Egidijus Gecius <e.gecius@gmail.com>
2020-04-11 18:55:04 +0100

    show how create a flow

--- app/src/test/java/com/egecius/coroutinesdemo/FlowTest.kt
+++ app/src/test/java/com/egecius/coroutinesdemo/FlowTest.kt
@@ -1,7 +1,17 @@
 package com.egecius.coroutinesdemo
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
+@InternalCoroutinesApi
 class FlowTest {
 
     @Test
@@ -16,5 +26,31 @@ class FlowTest {
 
         foo().forEach { value -> println(value) }
 
+    }
+
+    @Test
+    fun `show how create a flow`() = runBlockingTest {
+
+        fun foo(): Flow<Int> = flow { // flow builder
+            for (i in 1..3) {
+                println("flow thread: ${Thread.currentThread().name}")
+                delay(100) // pretend we are doing something useful here
+                emit(i) // emit next value
+            }
+        }
+
+        // Launch a concurrent coroutine to check if the main thread is blocked
+        launch {
+            for (k in 1..3) {
+                println("I'm not blocked $k")
+                delay(100)
+            }
+        }
+        // Collect the flow
+        foo().collect(object : FlowCollector<Int> {
+            override suspend fun emit(value: Int) {
+                println("result: $value")
+            }
+        })
     }
 }
