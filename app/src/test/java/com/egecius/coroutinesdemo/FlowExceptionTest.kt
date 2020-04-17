84e6d7b42c2e22e2227e818948be85843956e0c1
Egidijus Gecius <e.gecius@gmail.com>
2020-04-17 15:36:24 +0100

    adding 'catch' operator allows avoididng full try-catch block

--- app/src/test/java/com/egecius/coroutinesdemo/FlowExceptionTest.kt
+++ app/src/test/java/com/egecius/coroutinesdemo/FlowExceptionTest.kt
@@ -1,6 +1,8 @@
 package com.egecius.coroutinesdemo
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
@@ -8,15 +10,15 @@ import org.junit.Test
 
 class FlowExceptionTest {
 
+    private val flow: Flow<Int> = flow {
+        for (i in 1..3) {
+            println("Emitting $i")
+            emit(i) // emit next value
+        }
+    }
+
     @Test
     fun `you can catch exceptions thrown in collect block`() = runBlocking {
-        val flow: Flow<Int> = flow {
-            for (i in 1..3) {
-                println("Emitting $i")
-                emit(i) // emit next value
-            }
-        }
-
         try {
             flow.collect {
                 println(it)
@@ -25,5 +27,16 @@ class FlowExceptionTest {
         } catch (e: Exception) {
             println(e)
         }
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `adding 'catch' operator allows avoiding full try-catch block`() = runBlocking {
+        flow {
+            emit(1)
+            throw IllegalStateException()
+        }
+            .catch { e -> println(e) }
+            .collect()
     }
 } 
