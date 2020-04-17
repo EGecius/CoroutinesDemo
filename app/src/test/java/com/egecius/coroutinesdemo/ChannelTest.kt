cff3da4d817e4f15b74425a4411f9ccfbd3d8f64
Egidijus Gecius <e.gecius@gmail.com>
2020-04-17 16:45:45 +0100

    unlike a queue, a channel can be closed to indicate that no more elements are coming

--- app/src/test/java/com/egecius/coroutinesdemo/ChannelTest.kt
+++ app/src/test/java/com/egecius/coroutinesdemo/ChannelTest.kt
@@ -3,7 +3,6 @@ package com.egecius.coroutinesdemo
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 
 class ChannelTest {
@@ -17,6 +16,18 @@ class ChannelTest {
         }
         // here we print five received integers:
         repeat(5) { println(channel.receive()) }
+        println("Done!")
+    }
+
+    @Test
+    fun `unlike a queue, a channel can be closed to indicate that no more elements are coming`() = runBlocking {
+        val channel = Channel<Int>()
+        launch {
+            for (x in 1..5) channel.send(x * x)
+            channel.close() // we're done sending
+        }
+        // here we print received values using `for` loop (until the channel is closed)
+        for (y in channel) println(y)
         println("Done!")
     }
 }
