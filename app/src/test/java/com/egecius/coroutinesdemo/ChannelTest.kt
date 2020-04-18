cde940e02d27bae7ec810d2cc59b1dcd0c086733
Egidijus Gecius <e.gecius@gmail.com>
2020-04-18 08:26:36 +0100

    multiple coroutines may send to the same channel

--- app/src/test/java/com/egecius/coroutinesdemo/ChannelTest.kt
+++ app/src/test/java/com/egecius/coroutinesdemo/ChannelTest.kt
@@ -1,11 +1,10 @@
 package com.egecius.coroutinesdemo
 
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
@@ -46,5 +45,24 @@ class ChannelTest {
         }
         squaresChannel.consumeEach { println(it) }
         println("Done!")
+    }
+
+    @Test
+    fun `multiple coroutines may send to the same channel`() = runBlocking {
+        val channel = Channel<String>()
+        launch { sendString(channel, "foo", 200L) }
+        launch { sendString(channel, "BAR!", 500L) }
+        repeat(6) { // receive first six
+            println(channel.receive())
+        }
+        coroutineContext.cancelChildren() // cancel all children to let main finish
+
+    }
+
+    private suspend fun sendString(channel: SendChannel<String>, s: String, time: Long) {
+        while (true) {
+            delay(time)
+            channel.send(s)
+        }
     }
 }
