15aede2ac74e42dc94939e3efc12209fc123b05c
Egidjus Gecius <e.gecius@gmail.com>
2020-10-09 08:11:55 +0100

    fix tests in FlowToLiveDataTest

--- app/src/test/java/com/egecius/coroutinesdemo/FlowToLiveDataTest.kt
+++ app/src/test/java/com/egecius/coroutinesdemo/FlowToLiveDataTest.kt
@@ -2,13 +2,13 @@ package com.egecius.coroutinesdemo
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.asLiveData
+import com.egecius.coroutinesdemo.util.MainCoroutineRule
 import com.egecius.coroutinesdemo.util.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.test.runBlockingTest
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
@@ -16,9 +16,12 @@ import org.junit.Test
 class FlowToLiveDataTest {
 
     @get:Rule
-    val rule = InstantTaskExecutorRule()
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    @Ignore // TODO: 09/10/2020 fix failing test
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
     @Test
     fun `easy to append initial value to flow and covert to live data`() = runBlockingTest {
 
@@ -26,9 +29,8 @@ class FlowToLiveDataTest {
             .onStart { emit(0) }
             .asLiveData()
 
-        // TODO: 18/04/2020 learn how to test LiveData
         val result: Int? = liveData.getOrAwaitValue()
 
-        assertThat(result).isEqualTo(5)
+        assertThat(result).isEqualTo(0)
     }
 }
