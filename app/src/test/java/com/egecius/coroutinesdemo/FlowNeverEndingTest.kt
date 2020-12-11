3ee8d8c7f1998cd068f76bbc49e0c3687726b94f
Egidijus Gecius <e.gecius@gmail.com>
2020-12-11 07:28:31 +0000

    add a never ending test

--- app/src/test/java/com/egecius/coroutinesdemo/FlowNeverEndingTest.kt
+++ app/src/test/java/com/egecius/coroutinesdemo/FlowNeverEndingTest.kt
@@ -1,14 +1,22 @@
 package com.egecius.coroutinesdemo
 
 import com.egecius.coroutinesdemo.util.neverEndingEmittingFlow
+import com.egecius.coroutinesdemo.util.neverEndingEmptyFlow
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Ignore
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class FlowNeverEndingTest {
+
+    @Test @Ignore("will never complete")
+    fun `never ending empty flow will block the test`() = runBlockingTest {
+        // this will never complete & block the test
+        neverEndingEmptyFlow().collect {  }
+    }
     
     @Test
     fun `a never ending emitting flow can be terminated by using 'take'`() = runBlockingTest {
