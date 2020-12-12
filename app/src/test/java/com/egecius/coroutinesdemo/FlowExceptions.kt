068d9cd11adcc0f75b23fadac203d68ba715f805
Egidijus Gecius <e.gecius@gmail.com>
2020-12-12 10:11:36 +0000

    extract failing flow into a field

--- app/src/test/java/com/egecius/coroutinesdemo/FlowExceptions.kt
+++ app/src/test/java/com/egecius/coroutinesdemo/FlowExceptions.kt
@@ -2,10 +2,12 @@ package com.egecius.coroutinesdemo
 
 import app.cash.turbine.test
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.retry
 import kotlinx.coroutines.test.runBlockingTest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
 import org.junit.Test
 import kotlin.time.ExperimentalTime
 
@@ -13,24 +15,28 @@ import kotlin.time.ExperimentalTime
 @ExperimentalCoroutinesApi
 class FlowExceptions {
 
+    private var count = 0
+
+    private val failingFlow: Flow<Int> = flow {
+        if (count < 2) {
+            throw EgisException("demo 1")
+        } else {
+            throw RuntimeException("from in RuntimeException")
+        }
+    }
+
+    @Before
+    fun setup() {
+        count = 0
+    }
+
     @Test
     fun `'retry' allows retry if a certain condition is met`() = runBlockingTest {
-
-        var count = 0
-
-        val flow = flow<Int> {
-            if (count < 2) {
-                throw EgisException("demo 1")
-            } else {
-                throw RuntimeException("from in RuntimeException")
-            }
-        }.retry { throwable ->
+        failingFlow.retry { throwable ->
             count++
             // will retry if this condition is met
             throwable is EgisException
-        }
-
-        flow.test {
+        }.test {
             assertThat(expectError().message).isEqualTo("from in RuntimeException")
         }
 
