f4d58e595e49f22cad5857d06e1776f32d01a4f1
Egidijus Gecius <e.gecius@gmail.com>
2021-01-13 09:35:56 +0000

    rename to AsyncAwaitDemoTest

--- app/src/test/java/com/egecius/coroutinesdemo/AsyncAwaitDemoTest.kt
+++ app/src/test/java/com/egecius/coroutinesdemo/AsyncAwaitDemoTest.kt
@@ -6,7 +6,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
-class AwaitDemoTest {
+class AsyncAwaitDemoTest {
 
     @Test
     fun `async block gets executed before await() is called`() = runBlockingTest {
